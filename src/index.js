81801b7cc3dc48a6d19218f818f37ebbfffd8539
MdEstiakAhmed <estiak97@gmail.com>
2022-08-31 09:02:19 +0600

    add web vitals and menifest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './utils/reportWebVitals';
 import App from './app/App';
 import './assets/styles/style.css';
 
